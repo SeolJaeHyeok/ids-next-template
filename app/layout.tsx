9a2aef998dddc30edbc5cabee4ff611ee82d571c
Jaehyeok <milkboy2564@naver.com>
2023-10-19 21:27:15 +0900

    feat: Nextjs App Router에서 React Query를 사용하기 위한 기본 설정 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import RQProvider from '@/config/react-query/provider';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import React from 'react';
@@ -14,7 +15,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <RQProvider>{children}</RQProvider>
+            </body>
         </html>
     );
 }
