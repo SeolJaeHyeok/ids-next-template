bd059bd18dcc2dbfb783be2a7bf30a8227b495a7
Jaehyeok <milkboy2564@naver.com>
2023-10-19 21:45:29 +0900

    chore: Prettier 전체 적용 npx prettier --write **/*.{tsx.ts}

--- configs/react-query/query-key-factory.ts
+++ configs/react-query/query-key-factory.ts
@@ -1,6 +1,6 @@
 // react query key를 관리하는 factory 함수
 export const exampleKeys = {
-    all: ['articles'] as const,
-    lists: () => [...exampleKeys.all, 'get_all_articles'] as const,
+    all: ['example'] as const,
+    lists: () => [...exampleKeys.all] as const,
     list: (filters: any) => [...exampleKeys.lists(), { filters }] as const,
 };
