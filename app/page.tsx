77577ac465d99ac4d03775e8d128bda453f4dd0a
SeolJaeHyeok <milkboy2564@naver.com>
2024-05-20 14:02:01 +0900

    test3

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <main className={styles.main}>
             <div className={styles.description}>
                 <p>
-                    Get Test&nbsp;
+                    Get Test 2&nbsp;
                     <code className={styles.code}>app/page.tsx</code>
                 </p>
                 <div>
